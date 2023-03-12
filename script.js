e467c1f54025b72e2f3587588cba4e98ad1d8620
nmrtsnh <namratasinha26@gmail.com>
2023-03-12 23:44:19 +0100

    update

---
+++ script.js
@@ -0,0 +1,30 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
+let apiQuotes = [];
+
+// Show new quote
+function newQuote() {
+  //Pick random quote from apiquote array
+
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+
+async function getQuotes() {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    //catch error
+  }
+}
+
+// On Load
+getQuotes();
